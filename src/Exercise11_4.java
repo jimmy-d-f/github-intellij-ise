c93615918447e118187335c286e5acf72fb1ca4a
jimmy <jimmyfrosting@gmail.com>
2025-10-21 17:57:40 +0100

    init commit

---
+++ src/Exercise11_4.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Exercise11_4 {
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("DinoDiet.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("Error occurred while reading file: " + e.getMessage());
+        }
+
+    }
+}
