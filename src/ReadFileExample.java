d526613ee5cee3b1dc972c56a64d924e2fc765e7
jimmy <jimmyfrosting@gmail.com>
2025-10-21 17:59:36 +0100

    init commit

---
+++ src/ReadFileExample.java
@@ -0,0 +1,14 @@
+public class ReadFileExample {
+    public static void main(String[] args) {
+        try {
+            FileReader reader = new FileReader("input.txt");
+            int character;
+            while ((character = reader.read()) != -1) {
+                System.out.print((char) character);
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
