f53db8bc123e0f42a2c697d55b2a7ef0f902cf30
jimmy <jimmyfrosting@gmail.com>
2025-10-21 17:59:01 +0100

    init commit

---
+++ src/FileResourceCleanup.java
@@ -0,0 +1,22 @@
+public class FileResourceCleanup {
+    public static void main(String[] args) {
+        FileReader reader = null;
+        try {
+            reader = new FileReader("input.txt");
+            int character;
+            while ((character = reader.read()) !=-1){
+                System.out.print((char) character);
+            }
+        } catch (IOException e) {
+            System.out.println("Err: " + e.getMessage());
+        } finally {
+            if (reader != null){
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    System.out.println("Err closing: " + e.getMessage());
+                }
+            }
+        }
+    }
+}
